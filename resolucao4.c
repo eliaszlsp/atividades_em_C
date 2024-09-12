75b47508e37f03d146417a44a2080b32c7cc72f7
eliaszlsp <estudoselias@hotmail.com>
2024-09-11 23:39:18 -0300

    1_ao_4

---
+++ resolucao4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    int NumeroBase10;
+
+    char hexadecimal[10];
+
+    printf("Digite um número em base 10 para a conversão em base 16: ");
+    scanf("%d", &NumeroBase10);
+
+    sprintf(hexadecimal, "%X", NumeroBase10);
+
+    printf("O número em Hexadecimal é %s \n", hexadecimal);
+
+    return 0;
+}
